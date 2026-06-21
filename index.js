0e4831af201ff5867bb0631383ccc638c195997d
sharifjanof <azwebdanater@gmail.com>
2026-06-21 16:14:38 +0500

    added

--- index.js
+++ index.js
@@ -20,12 +20,17 @@ const advData = {};
 
 const CHECK_CHANNELS = [
     "@seoulflixorg",
+    "@Sabr_voices"
 ];
 
 const SHOW_CHANNELS = [
     {
         name: "1 - Asosiy kanal",
         url: "https://t.me/seoulflixorg"
+    },
+    {
+        name: "1 - Asosiy kanal",
+        url: "https://t.me/Sabr_voices"
     },
 
 ];
