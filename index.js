b866934e174496be9648471bfdce81a78637ef96
sharifjanof <azwebdanater@gmail.com>
2026-06-24 00:41:51 +0500

    added

--- index.js
+++ index.js
@@ -18,10 +18,9 @@ const db = admin.firestore();
 const userStates = {};
 const advData = {};
 
-const CHECK_CHANNELS = ["@seoulflixorg", "@izohsiz_822"];
+const CHECK_CHANNELS = ["@seoulflixorg"];
 const SHOW_CHANNELS = [
     { name: "1 - Asosiy kanal", url: "https://t.me/seoulflixorg" },
-    { name: "2 - Asosiy kanal", url: "https://t.me/izohsiz_822" },
 ];
 
 // ─────────────────────────────────────────────
